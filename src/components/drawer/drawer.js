390cbfc80ea20d90f9c9c4647d2d69bde8322480
ColoSRJones <colosrjones@gmail.com>
2019-11-05 22:28:06 -0600

    McFly needs help on drawer stuff

--- src/components/drawer/drawer.js
+++ src/components/drawer/drawer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './drawer.scss';
+import '../../styles/core/_drawer.scss';
 
 const Drawer = props => {
   return <aside>{props.children}</aside>;
@@ -9,5 +9,6 @@ const Drawer = props => {
 export default Drawer;
 
 Drawer.defaultProps = {
-  children: [<div>Stuff</div>],
+  children: [<div>Steven Jones doesn't know much about drawer. Marty McFly's friend, Doc set up time for back to the future. But there is accident hit on November 5, 1955. Marty McFly got away from terriorist and he jumped to time travel car. Car got away and went to year 1955! 
+</div>],
 };
