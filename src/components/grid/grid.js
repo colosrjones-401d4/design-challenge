8b612b34cc9974e50aedf9969214119eacb645fd
ColoSRJones <colosrjones@gmail.com>
2019-11-09 08:55:04 -0600

    grid styles

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -4,18 +4,15 @@ import '../../styles/core/_grid.scss';
 
 const Grid = props => {
   return (
-<section>
-  <div class="wrapper">
-   <div class="one">One</div>
-   <div>Two</div>
-   <div>Three</div>
-   <div>Four</div>
-   <div class="one">Five</div>
-   <div>Six</div>
-   <div>Seven</div>
-   <div>Eight</div>
- </div>
-</section>
+<div class="wrapper">
+  <header class="header">Header</header>
+  <article class="main">
+    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>  
+  </article>
+  <aside class="aside aside-1">Aside 1</aside>
+  <aside class="aside aside-2">Aside 2</aside>
+  <footer class="footer">Footer</footer>
+</div>
   );
 };
 
